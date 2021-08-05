818cc6e317e7468abeb15fc5eacf7464a7392bc6
Dima <dima@xp.network>
2021-08-05 23:33:09 +0300

    add: radio buttons

--- src/SelectAsset/parts/checkbox.jsx
+++ src/SelectAsset/parts/checkbox.jsx
@@ -1,6 +1,6 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import CheckBoxOn from "../../assets/SVG/selectAssets/check_box.svg"
-export default function Checkbox({cb, unselectCb}){
+export default function Checkbox({ cb, unselectCb }) {
     const [clickCheckBox, setClickCheckBox] = useState(true);
 
     const onclick = () => {
@@ -8,7 +8,12 @@ export default function Checkbox({cb, unselectCb}){
         (clickCheckBox && cb) && cb();
         (!clickCheckBox && unselectCb) && unselectCb();
     };
-    return <div onClick={onclick} className="select-all-check-box-none">
-        {clickCheckBox ? "" : <img src={CheckBoxOn} alt="checkOn"/>}
+    return <div
+        onClick={onclick}
+        className="select-all-check-box-none"
+        checked={clickCheckBox}
+
+    >
+        {clickCheckBox ? "" : <div  className="inner-radio-box"/>}
     </div>
 }
