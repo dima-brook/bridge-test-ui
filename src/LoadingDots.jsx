a8ecb427ba0d15b06e1f0aa08b7e5bcfa51234e8
Dima <dima@xp.network>
2021-08-08 20:07:18 +0300

    add: spinner

---
+++ src/LoadingDots.jsx
@@ -0,0 +1,16 @@
+
+const LoadingDots = () => {
+
+
+    return (
+        <div class="spinner-box">
+            <div class="pulse-container">
+                <div class="pulse-bubble pulse-bubble-1"></div>
+                <div class="pulse-bubble pulse-bubble-2"></div>
+                <div class="pulse-bubble pulse-bubble-3"></div>
+            </div>
+        </div>
+    )
+}
+
+export default LoadingDots;
